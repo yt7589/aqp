7fdf45b408fa5cc134fa031ccc106c8db6ce9c98
yt7589 <yt7589@qq.com>
2019-04-11 18:16:03 +0800

    remove no use import from app_main.py

--- app_main.py
+++ app_main.py
@@ -3,18 +3,8 @@ sys.path.append('./core')
 import numpy as np
 #import tensorflow as tf
 from app_registry import appRegistry as ar
-from controller.c_stock import CStock
-from controller.c_stock_daily import CStockDaily
-from ann.svm import Svm
 #import model.m_mysql as db
-from util.stock_daily_svm_model_evaluator import StockDailySvmModelEvaluator
-from controller.c_portfolio import CPortfolio
-from ann.linear_regression_engine import LinearRegressionEngine
-from app.kelly_demo import KellyDemo
-from util.learn import Learn
 from app.stock_backtest import StockBacktest
-from controller.c_account import CAccount
-
 import app.qh.qh_main as qh
 import app.pqb.pqb_main as pqb
 
