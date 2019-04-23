0ab30729db6093dd95ccd29c77b1d4b84def6b14
yt7589 <yt7589@qq.com>
2019-04-23 19:44:19 +0800

    get banks quotation data from baostock

--- app/tpsa/tpsa_main.py
+++ app/tpsa/tpsa_main.py
@@ -6,5 +6,17 @@ class TpsaMain(object):
         
     def startup(self):
         print('A股交易对策略')
+        # self.get_quotation_data() # 获取行情数据
+        
+    def get_quotation_data(self):
+        # 取工商银行2016-10-27~2019-04-23
         bs_cna_daily = BsCnaDaily()
-        bs_cna_daily.get_history_data('sh.601398', '2017-01-01', '2017-12-31')
+        # 工商银行（股票代码60开头的是上海）
+        bs_cna_daily.get_history_data('sh.601398', 
+                    '2008-01-01', '2019-04-23')
+        # 建设银行
+        bs_cna_daily.get_history_data('sh.601939', 
+                    '2008-01-01', '2019-04-23')
+        # 浦发银行
+        bs_cna_daily.get_history_data('sh.600000', 
+                    '2008-01-01', '2019-04-23')
