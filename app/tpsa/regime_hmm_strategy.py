17ae51767585a1c7f19aeb0d1e4d7dd094e1e32d
yt7589 <yt7589@qq.com>
2019-05-03 14:56:20 +0800

    add hidden markov model regime strategy

---
+++ app/tpsa/regime_hmm_strategy.py
@@ -0,0 +1,66 @@
+# regime_hmm_strategy.py
+
+from __future__ import print_function
+
+from collections import deque
+
+import numpy as np
+
+from qstrader.price_parser import PriceParser
+from qstrader.event import (SignalEvent, EventType)
+from qstrader.strategy.base import AbstractStrategy
+
+class RegimeHmmStrategy(AbstractStrategy):
+    def __init__(self, tickers, 
+                events_queue, base_quantity,
+                short_window=10, long_window=30):
+        '''
+        初始化隐马可夫市场机制转换趋势跟踪策略
+        @param events_queue 系统事件队列
+        @param base_quantity 
+        @param short_window 近期移动平均窗口大小
+        @param long_window 长期移动平均窗口大小
+        '''
+        self.name = 'RegimeHmmStrategy'
+        self.tickers = tickers
+        self.events_queue = events_queue
+        self.base_quantity = base_quantity
+        self.short_window = short_window
+        self.long_window = long_window
+        self.bars = 0
+        self.invested = False
+        self.sw_bars = deque(maxlen=self.short_window)
+        self.lw_bars = deque(maxlen=self.long_window)
+
+    def calculate_signals(self, event):
+        # Applies SMA to first ticker
+        ticker = self.tickers[0]
+        if event.type == EventType.BAR and event.ticker == ticker:
+            # Add latest adjusted closing price to the
+            # short and long window bars
+            price = event.adj_close_price/float(
+                PriceParser.PRICE_MULTIPLIER
+            )
+            self.lw_bars.append(price)
+            if self.bars > self.long_window - self.short_window:
+                self.sw_bars.append(price)
+
+            # Enough bars are present for trading
+            if self.bars > self.long_window:
+                # Calculate the simple moving averages
+                short_sma = np.mean(self.sw_bars)
+                long_sma = np.mean(self.lw_bars)
+                # Trading signals based on moving average cross
+                if short_sma > long_sma and not self.invested:
+                    print("LONG: %s" % event.time)
+                    signal = SignalEvent(ticker, "BOT", self.base_quantity)
+                    self.events_queue.put(signal)
+                    self.invested = True
+                elif short_sma < long_sma and self.invested:
+                    print("SHORT: %s" % event.time)
+                    signal = SignalEvent(ticker, "SLD", self.base_quantity)
+                    self.events_queue.put(signal)
+                    self.invested = False
+            self.bars += 1
+        
+    
