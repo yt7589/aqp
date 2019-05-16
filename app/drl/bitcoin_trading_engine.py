4d9053c77cc48a6e40618b8ea4e6aead6de15b70
yantao <yt7589@qq.com>
2019-05-16 18:59:04 +0800

    Add bitcoin trading deep reinforcement learing quant trading research environment

---
+++ app/drl/bitcoin_trading_engine.py
@@ -0,0 +1,60 @@
+import os
+import gym
+import pandas as pd
+
+from stable_baselines.common.policies import MlpPolicy
+from stable_baselines.common.vec_env import DummyVecEnv
+from stable_baselines import A2C
+
+from app.drl.bitcoin_trading_env import BitcoinTradingEnv
+
+class BitcoinTradingEngine(object):
+    def __init__(self):
+        self.name = 'BitcoinTradingEngine'
+        self.model_file = './work/bitcoin_drl.a2c'
+        self.train_steps = 200
+        self.test_size = 100
+        self.first_run = False
+
+    def startup(self):
+        print('深度强化学习比特币量化交易平台 v0.0.1')
+        model = self.train()
+        self.evaluate(model)
+
+    def build_train_env(self):
+        df = pd.read_csv('./data/bitstamp.csv')
+        df = df.sort_values('Timestamp')
+        self.slice_point = int(len(df) - self.test_size)
+        self.train_df = df[:self.slice_point]
+        self.test_df = df[self.slice_point:]
+        return DummyVecEnv(
+            [lambda: BitcoinTradingEnv(self.train_df, serial=True)])
+
+    def build_model(self, train_env):
+        ''' build model for the very first time '''
+        return A2C(MlpPolicy, train_env, verbose=1,
+                tensorboard_log="./tensorboard/")
+
+    def train(self):
+        train_env = self.build_train_env()
+        if self.first_run:
+            model = self.build_model(train_env)
+        else:
+            print('装入已有训练模型...')
+            model = A2C.load(self.model_file)
+            model.set_env(train_env)
+        model.learn(total_timesteps=self.train_steps)
+        model.save(self.model_file)
+        train_env.close()
+        return model
+
+    def evaluate(self, model):
+        test_env = DummyVecEnv(
+                [lambda: BitcoinTradingEnv(self.test_df, serial=True)])
+        obs = test_env.reset()
+        for i in range(self.test_size):
+            action, _states = model.predict(obs)
+            obs, rewards, done, info = test_env.step(action)
+            test_env.render(mode="human", title="BTC")
+
+        test_env.close()
--- app/drl/drl_main.py
+++ app/drl/drl_main.py
@@ -1,10 +1,13 @@
 #
 from app.drl.holt_winters import HoltWinters
+from app.drl.bitcoin_trading_engine import BitcoinTradingEngine
 
 class DrlMain(object):
     def __init__(self):
         self.name = 'DrlMain'
         
     def startup(self):
-        holt_winters = HoltWinters()
-        holt_winters.startup()
+        #holt_winters = HoltWinters()
+        #holt_winters.startup()
+        engine = BitcoinTradingEngine()
+        engine.startup()
