29d3082a4008d27d641ec722cd6b41461ee5dd6f
Yantao <yt7589@qq.com>
2019-06-21 15:01:05 +0800

    design normalization algorithm in agent choose_action method

--- tdd/app/fme/t_fme_xgb_agent.py
+++ tdd/app/fme/t_fme_xgb_agent.py
@@ -1,4 +1,6 @@
 import unittest
+import numpy as np
+import xgboost as xgb
 from app.fme.fme_dataset import FmeDataset
 from app.fme.fme_xgb_agent import FmeXgbAgent
 
@@ -11,3 +13,19 @@ class TFmeXgbAgent(unittest.TestCase):
         #fme_dataset.create_bitcoin_dataset(dataset_size=10000)
         fme_xgb_agent = FmeXgbAgent()
         fme_xgb_agent.train_baby_agent()
+
+    def test_bug(self):
+        fme_xgb_agent = FmeXgbAgent()
+        fme_dataset = FmeDataset()
+        X, y = fme_dataset.load_bitcoin_dataset()
+        self.predict(fme_xgb_agent, X, 0)
+        self.predict(fme_xgb_agent, X, 4)
+        self.predict(fme_xgb_agent, X, 8)
+        self.predict(fme_xgb_agent, X, 9)
+        self.predict(fme_xgb_agent, X, 10)
+
+    def predict(self, fme_xgb_agent, X, idx):
+        x1 = np.array([X[idx]])
+        xg1 = xgb.DMatrix( x1, label=x1)
+        pred = fme_xgb_agent.model.predict( xg1 )
+        print('预测结果{0}：{1}'.format(idx, np.argmax(pred)))
